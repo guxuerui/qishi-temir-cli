3e4157c76b50ffe7a3a8c12f0146aadd0a7d73a6
guxuerui <im.guxuerui@gmail.com>
2022-09-06 18:31:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { TBox, TText } from '@temir/core'
+const count = ref(0)
+setInterval(() => {
+  count.value++
+}, 100)
+</script>
+
+<template>
+  <TBox
+    :margin="5"
+    :width="30"
+    :height="5"
+    border-style="double"
+    justify-content="center"
+    align-items="center"
+  >
+    <TText color="#42b883">
+      🌈 Hi TEMIR !! {{ count }}
+    </TText>
+  </TBox>
+</template>
