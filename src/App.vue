bbfac0cdfb717b9c3f89c8d25c91f4c485461902
guxuerui <im.guxuerui@gmail.com>
2022-09-06 22:19:51 +0800

    feat: use new example

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,49 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { TBox, TText } from '@temir/core'
+import { TBox, TSpacer, TText } from '@temir/core'
+import TLink from '@temir/link'
 const count = ref(0)
 setInterval(() => {
   count.value++
-}, 100)
+}, 10 * 1000)
 </script>
 
 <template>
   <TBox
     :margin="5"
     :width="30"
-    :height="5"
+    :min-height="10"
     border-style="double"
     justify-content="center"
     align-items="center"
+    flex-direction="column"
   >
-    <TText color="#42b883">
-      🌈 Hi TEMIR !! {{ count }}
+    <TText bold italic underline color="black" background-color="green">
+      I am black on green
     </TText>
+    <TBox :padding="1" border-style="classic" border-color="green">
+      <TText color="#42b883">
+        🌈 Hi TEMIR !! {{ count }}
+      </TText>
+    </TBox>
+  </TBox>
+  <TBox flex-direction="column" :height="10" border-style="double" border-color="greenBright">
+    <TText>Top</TText>
+    <TSpacer />
+    <TText>Bottom</TText>
+  </TBox>
+
+  <TBox flex-direction="row" :height="10" border-style="double" border-color="yellowBright">
+    <TText>Left</TText>
+    <TSpacer />
+    <TText>Right</TText>
+    <TLink url="https://github.com">
+      <TText color="yellow">
+        Hi
+      </TText>
+      <TText color="cyan">
+        Github
+      </TText>
+    </TLink>
   </TBox>
 </template>
